57a820aac6cf7b905b70236b05bbf0c9b955ef93
hennyrose <93723100+hennyrose@users.noreply.github.com>
2025-01-07 16:39:09 +0200

    Update Main.java

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,4 +1,11 @@
 package main.java.game;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        Scanner myChoice = new Scanner(System.in);
+        game.start();
+
+    }
 }
