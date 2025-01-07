b60a58752eb1e0f049ba55d01cdb34e39e8807a5
hennyrose <93723100+hennyrose@users.noreply.github.com>
2025-01-07 16:38:53 +0200

    Update Player.java

--- src/main/java/game/Player.java
+++ src/main/java/game/Player.java
@@ -1,4 +1,19 @@
 package main.java.game;
 
 public class Player {
+    private String name;
+    private House house;
+
+    public Player(String name, House house) {
+        this.name = name;
+        this.house = house;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public House getHouse() {
+        return house;
+    }
 }
